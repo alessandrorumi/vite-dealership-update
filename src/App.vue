4a9b5e51c16abcaad316ed7966ba3fa613ef62c2
alessandrorumi <rumi.alessandro21@gmail.com>
2023-12-28 09:16:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Home from './components/Home.vue'
+import About from './components/About.vue'
+</script>
+
+<template>
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">About</router-link>
+  <router-view></router-view>
+</template>
+
+
+<style lang="scss"></style>
---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<template>
+  <h1>Altro</h1>
+</template>
+
+<style lang="scss" scoped></style>
